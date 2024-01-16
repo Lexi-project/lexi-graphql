86404e54c773763f693d023a53326ee8f62e13f8
anup-an <friek_an@yahoo.com>
2024-01-16 12:09:58 +0200

    Add resolver to handle query to get exercise

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,8 +1,28 @@
-import type { Resolvers } from "./resolvers-types";
+import type { GetExerciseContentResponse, Resolvers } from "./resolvers-types";
+import clients from "./client";
 
 const resolvers: Resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    exercise: async (parent, args, context, info) => {
+      try {
+        const response = await new Promise((resolve, reject) => {
+          clients.exerciseGrpcClient.GetExerciseContent(
+            args.input,
+            (error, response: GetExerciseContentResponse) => {
+              if (error) {
+                reject(error);
+                throw new Error('Failed to create exercise');
+              } else {
+                resolve(response);
+              }
+            }
+          );
+        });
+        return response;
+      } catch (error: unknown) {
+        throw new Error("Failed to create exercise");
+      }
+    },
   },
 };
 
