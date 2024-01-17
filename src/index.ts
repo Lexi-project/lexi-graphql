d884cb7329cc1699a2f465189a859c670da0a7bb
anup-an <friek_an@yahoo.com>
2024-01-17 02:11:49 +0200

    Update queries and mutations

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import { ApolloServer } from "@apollo/server";
 import { readFileSync } from "fs";
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { startStandaloneServer } from "@apollo/server/standalone";
 import resolvers from "./resolvers";
 
-
-const typeDefs = readFileSync('./src/schema.graphql', { encoding: 'utf-8' });
+const typeDefs = readFileSync("./src/schema.graphql", { encoding: "utf-8" });
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  status400ForVariableCoercionErrors: true,
 });
 
 const { url } = await startStandaloneServer(server, {
+  context: async ({req, res}) => {
+    return {
+      resolvers,
+      authToken: req.headers.authorization
+    }
+  },
   listen: { port: 4000 },
 });
 
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,26 +1,85 @@
-import type { GetExerciseContentResponse, Resolvers } from "./resolvers-types";
-import clients from "./client";
+import type { BorrowCreditResponse, Resolvers } from "./resolvers-types";
+import client from "./client";
+import { GraphQLError } from "graphql";
 
 const resolvers: Resolvers = {
   Query: {
     exercise: async (parent, args, context, info) => {
+      let borrowCredit: BorrowCreditResponse = {};
       try {
-        const response = await new Promise((resolve, reject) => {
-          clients.exerciseGrpcClient.GetExerciseContent(
-            args.input,
-            (error, response: GetExerciseContentResponse) => {
-              if (error) {
-                reject(error);
-                throw new Error('Failed to create exercise');
-              } else {
-                resolve(response);
-              }
-            }
-          );
-        });
-        return response;
+        const borrow = await context.resolvers.Mutation.BorrowCredit(
+          parent,
+          {
+            input: { token: context.authToken, credits: 1 },
+          },
+          context,
+          info
+        );
+        borrowCredit = borrow;
+        const exercise = await client.callGrpcMethod<typeof args>(
+          client.exerciseGrpcClient,
+          "GetExerciseContent",
+          args
+        );
+        await context.resolvers.Mutation.CommitCredit(
+          parent,
+          {
+            input: { transaction_id: borrow.transaction_id },
+          },
+          context,
+          info
+        );
+        return exercise;
       } catch (error: unknown) {
-        throw new Error("Failed to create exercise");
+        if (borrowCredit.transaction_id) {
+          await context.resolvers.Mutation.RollbackCredit(
+            parent,
+            {
+              input: {
+                token: context.authToken,
+                transaction_id: borrowCredit.transaction_id,
+              },
+            },
+            context,
+            info
+          );
+        }
+        throw new GraphQLError("Failed to generate content");
+      }
+    },
+  },
+  Mutation: {
+    BorrowCredit: async (parent, args, context, info) => {
+      try {
+        return await client.callGrpcMethod<typeof args.input>(
+          client.creditGrpcClient,
+          "BorrowCredit",
+          args.input
+        );
+      } catch (error) {
+        throw new GraphQLError("Failed to borrow credit");
+      }
+    },
+    CommitCredit: async (parent, args, context, info) => {
+      try {
+        return await client.callGrpcMethod<typeof args.input>(
+          client.creditGrpcClient,
+          "CommitCredit",
+          args.input
+        );
+      } catch (error) {
+        throw new GraphQLError("Failed to commit credit");
+      }
+    },
+    RollbackCredit: async (parent, args, context, info) => {
+      try {
+        return await client.callGrpcMethod<typeof args.input>(
+          client.creditGrpcClient,
+          "RollbackCredit",
+          args.input
+        );
+      } catch (error) {
+        throw new GraphQLError("Failed to rollback credit");
       }
     },
   },
